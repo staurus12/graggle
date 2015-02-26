50115a238d60b40157bfe78ac02dd5dcec277fb4
staurus12 <staurus12@gmail.com>
2015-02-25 23:55:42 -0800

    Introduced angular-js

---
+++ trialCode/index-angular.js
@@ -0,0 +1,13 @@
+function graggleController($scope){
+
+$scope.trendingGraggles = [ 
+					{name:"Milk Gallons", location:"Sunnyvale", state:"CA"},
+				    {name:"BMW 3 series", location:"Palo Elto", state:"CA"},
+					{name:"Bread", location:"Sunnyvale", state:"CA"},
+					{name:"Honda Accord", location:"Mountain View", state:"CA"},
+					{name:"Gas", location:"Sunnyvale", state:"CA"}
+					      ];
+					   
+//$scope.trendingGraggles = trendingGraggles;				   
+$scope.totalTrending = 5;
+}
